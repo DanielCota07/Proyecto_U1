24fa5ea1077c7585552a3eb85175138aba6ca2b6
Daniel Cota <danielalbertocotaochoa6@gmail.com>
2021-03-16 18:45:13 -0700

    Se agergo el submeno Agile

--- Proyecto.sh
+++ Proyecto.sh
@@ -1,11 +1,25 @@
-#!/bin/bash
+      #!/bin/bash
 
 case $1 in
         -a )
-        echo "Menú Agile"
+        echo "Bienvenido a la guia rapido de Agile, para continuar seleccione un tema: "
+        echo "1. SCRUM";
+        echo "2. X.P.";
+        echo "3. Kanban";
+        echo "4. Crystal"
+	while true; do
+   	read -p"Ingrese la opcion que desea: " op
+    	case $op in
+        	[1]* ) echo "SCRUM"; break;;
+        	[2]* ) echo "X.P."; break;;
+        	[3]* ) echo "Kanban"; break;;
+        	[4]* ) echo "Crystal"; break;;
+        	*) echo "Seleccione una Opción de 1 a 4.";;
+    esac
+done
           ;;
         -t )
-        echo "Menú Tradicionales"
+        echo "Bienvenido a la guia rapido de Metodologia tradicionales, para continuar seleccione un tema: "
           ;;
         * )
         echo "No entiendo lo que me dices"
