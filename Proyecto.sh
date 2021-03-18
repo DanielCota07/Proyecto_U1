e134339c1cc501757dd752c03e5f08e83ab41129
Axel Dalí <a329881@uach.mx>
2021-03-17 20:11:25 -0700

    Se agrega la función de borrar() y se modifican los menús

--- Proyecto.sh
+++ Proyecto.sh
@@ -21,34 +21,51 @@ menuTradicionales(){
 }
 
 menuInfo(){
-    echo "Usted esta en la sección $1, seleccione la opción que desea utilizar."
-    echo "1. Agregar información";
-    echo "2. Buscar";
-    echo "3. Eliminar información";
-    echo "4. Leer base de información"
-    while true; do
-        read -p"Ingrese la acción que desea realizar: " opInfo
-        case $opInfo in
-        [1]* ) echo "Agregar información";
-            agregarInformacion "$2";
-            break;;
-        [2]* ) echo "Buscar";
-            buscar "$2";
-            break;;
-        [3]* ) echo "Eliminar información";
-            eliminarInformación "$2";
-            break;;
-        [4]* ) echo "Leer base de información";
-            leerBaseDeInfo "$2";
-            break;;
-        *) echo "Seleccione una Opción de 1 a 4.";;
-        esac
+    salir=0
+    while [ $salir -ne 1 ]; do
+      clear
+      echo "Usted esta en la sección $1, seleccione la opción que desea utilizar."
+      echo "1. Agregar información";
+      echo "2. Buscar";
+      echo "3. Eliminar información";
+      echo "4. Leer base de información";
+        while true; do
+            read -p "Ingrese la acción que desea realizar: " opInfo
+            case $opInfo in
+            [1]* ) echo "Agregar información";
+                agregarInformacion "$2";
+                break;;
+            [2]* ) echo "Buscar";
+                buscar "$2";
+                break;;
+            [3]* ) echo "Eliminar información";
+                eliminarInformación "$2";
+                break;;
+            [4]* ) echo "Leer base de información";
+                leerBaseDeInfo "$2";
+                break;;
+            *) echo -e "\nSeleccione una Opción de 1 a 4." ;;
+            esac
+        done
+        clear;
+        echo "Seleccione la opción que desea."
+        echo "0. Regresar al menú anterior";
+        echo "1. Salir de la aplicación";
+        while true; do
+            read -p "Ingrese la acción que desea realizar: " salir
+            case $salir in
+            [0]* ) break;;
+            [1]* ) break;;
+            *) echo "Seleccione una opción válida.";;
+            esac
+        done
   done
 }
 
 agregarInformacion(){
+    echo -e "\nAgregar Información";
     file="$1"
-    echo $file
+    #echo $file
     if [ ! -f "$file" ]
     then
       touch $file
@@ -59,15 +76,17 @@ agregarInformacion(){
 }
 
 buscar(){
-    echo "Buscar";
+    echo -e "\nBuscar"
 }
 
 eliminarInformación(){
-    echo "Eliminar";
+    echo -e "\nEliminar";
+    read -p "Ingrese el concepto que desea eliminar: " concepto
+    sed -i "/$concepto/, +1 d" $1
 }
 
 leerBaseDeInfo(){
-    echo "Leer";
+    echo -e "\nLeer";
 }
 
 
@@ -92,7 +111,7 @@ while true; do
        [4]* ) echo "Crystal";
               menuInfo "Crystal" "./Archivos/Agile/crystal.inf";
               break;;
-       *) echo "Seleccione una Opción de 1 a 4.";;
+       *) echo -e "\nSeleccione una Opción de 1 a 4." ;;
    esac
 done
 ;;
@@ -110,7 +129,7 @@ while true; do
       [3]* ) echo "Modelo V";
              menuInfo "Modelo V" "./Archivos/Tradicionales/scrum.inf";
              break;;
-      *) echo "Seleccione una opción de 1 a 3.";;
+      *) echo -e "\nSeleccione una opción de 1 a 3.";;
   esac
 done
 ;;
